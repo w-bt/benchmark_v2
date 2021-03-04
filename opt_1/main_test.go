ab24c4719da930782b0f2f6f246bbc3410c364d5
Widhian Bramantya <widhian.bramantya@go-jek.com>
2021-03-04 23:43:30 +0700

    refactor(main_test.go): refactor test

--- opt_1/main_test.go
+++ opt_1/main_test.go
@@ -70,7 +70,7 @@ import (
 //	}
 //}
 
-// go test -v -bench=. -benchtime=30000x -cpuprofile=prof.cpu -memprofile=prof.mem -o bench.test | tee prof
+// go test -v -bench=. -benchtime=30000x -cpuprofile=prof.cpu -memprofile=prof.mem -o bench.test | tee prof.txt
 // use -gcflags "-m -l" to do escape analysis
 // go tool pprof bench.test prof.cpu
 // go tool pprof bench.test prof.mem
@@ -78,9 +78,9 @@ import (
 // go tool pprof -http=":5002" bench.test prof.mem
 func BenchmarkHandleProduct(b *testing.B) {
 	b.ReportAllocs()
+	r, _ := http.ReadRequest(bufio.NewReader(strings.NewReader("GET /product?code=ZZ99 HTTP/1.0\r\n\r\n")))
+	rw := httptest.NewRecorder()
 	for i := 0; i < b.N; i++ {
-		r, _ := http.ReadRequest(bufio.NewReader(strings.NewReader("GET /product?code=ZZ99 HTTP/1.0\r\n\r\n")))
-		rw := httptest.NewRecorder()
 		handleProduct(rw, r)
 		rw.Body.Reset()
 	}
--- opt_3/main_test.go
+++ opt_3/main_test.go
@@ -8,18 +8,18 @@ import (
 	"testing"
 )
 
-// go test -v -bench=. -benchtime=30000x -cpuprofile=prof.cpu -memprofile=prof.mem -o bench.test | tee prof
+// go test -v -bench=. -benchtime=30000x -cpuprofile=prof.cpu -memprofile=prof.mem -o bench.test | tee prof.txt
 // use -gcflags "-m -l" to do escape analysis
 // go tool pprof bench.test prof.cpu
 // go tool pprof bench.test prof.mem
 // go tool pprof -http=":5001" bench.test prof.cpu
 // go tool pprof -http=":5002" bench.test prof.mem
-// benchcmp prof ../opt_2/prof
+// benchstat prof.txt ../opt_2/prof.txt
 func BenchmarkHandleProduct(b *testing.B) {
 	b.ReportAllocs()
+	r, _ := http.ReadRequest(bufio.NewReader(strings.NewReader("GET /product?code=ZZ99 HTTP/1.0\r\n\r\n")))
+	rw := httptest.NewRecorder()
 	for i := 0; i < b.N; i++ {
-		r, _ := http.ReadRequest(bufio.NewReader(strings.NewReader("GET /product?code=ZZ99 HTTP/1.0\r\n\r\n")))
-		rw := httptest.NewRecorder()
 		handleProduct(rw, r)
 		rw.Body.Reset()
 	}
